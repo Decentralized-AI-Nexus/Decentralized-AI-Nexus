97710bcdefcd0f4c5fc8ab328fc73c8e5b4edd44
Decentralized-AI-Nexus <83626276+Decentralized-AI-Nexus@users.noreply.github.com>
2025-05-11 21:40:02 +0800

    Update appdirs.py

--- pkg_resources/_vendor/appdirs.py
+++ pkg_resources/_vendor/appdirs.py
@@ -96,7 +96,26 @@ def user_data_dir(appname=None, appauthor=None, version=None, roaming=False):
         path = os.path.join(path, version)
     return path
 
+    def __init__(self, parent, id=-1):
+        super(MatplotlibPanel, self).__init__(parent, id)
+        self.TopBoxSizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(self.TopBoxSizer)
 
+        self.btn_bkt = wx.Button(self, label="", pos=(100, 10))
+
+        self.figure = matplotlib.figure.Figure(figsize=(4, 3))
+        self.figure = plt.figure(figsize=(4, 3))
+        self.canvas = FigureCanvas(self, -1, self.figure)
+        self.TopBoxSizer.Add(
+            self.canvas, proportion=-10, border=2, flag=wx.ALL | wx.EXPAND
+        )
+ def output_earning_rate(self):
+        df = self.bars[-self.days :]
+        df["signals"] = self.signals
+        df["strategy"] = (1 + df.close.pct_change(1).fillna(0) * self.signals).cumprod()
+        df["base"] = df["close"] / df["close"][0]
+        print(df["strategy"].values[-1:])
+        return df
 def site_data_dir(appname=None, appauthor=None, version=None, multipath=False):
     r"""Return full path to the user-shared data dir for this application.
 
