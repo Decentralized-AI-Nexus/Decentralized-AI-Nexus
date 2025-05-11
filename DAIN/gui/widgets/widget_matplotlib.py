55584b29c8649063c9fe36d19c99e296849425b8
Decentralized-AI-Nexus <83626276+Decentralized-AI-Nexus@users.noreply.github.com>
2025-05-11 21:38:35 +0800

    Update widget_matplotlib.py

--- DAIN/gui/widgets/widget_matplotlib.py
+++ DAIN/gui/widgets/widget_matplotlib.py
@@ -18,6 +18,37 @@ class MatplotlibPanel(wx.ScrolledWindow):
         self.TopBoxSizer.Add(
             self.canvas, proportion=-10, border=2, flag=wx.ALL | wx.EXPAND
         )
+ def output_earning_rate(self):
+        df = self.bars[-self.days :]
+        df["signals"] = self.signals
+        df["strategy"] = (1 + df.close.pct_change(1).fillna(0) * self.signals).cumprod()
+        df["base"] = df["close"] / df["close"][0]
+        print(df["strategy"].values[-1:])
+        return df
 
+    def show_plt(self):
+        df = self.output_earning_rate()
+        fig, axes = plt.subplots(2, 1, sharex=True, figsize=(18, 12))
+        df[["strategy", "base", "signals"]].plot(
+            ax=axes[0], grid=True, title="收益", figsize=(20, 10)
+        )
+        self.show_score(df, axes[1])
+        plt.show()
+
+    def show_score(self, df, ax):
+        df["score"] = self.get_scores(df)
+        df.score.plot(ax=ax, grid=True, title="score", figsize=(20, 10))
+
+    def process(self):
+        position = 0
+        for i in range(self.days):
+
+            singal = self.get_singal(self.bars[: -self.days + i - 1])
+            if singal == -1:
+
+                self.signals.append(position)
+            else:
+                position = singal
+                self.signals.append(singal)
     def show(self):
         plt.show()
